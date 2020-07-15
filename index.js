3eeddabc20d8095270bfc9913874db0b7b93ff9d
tober65 <timothy.ober@gmail.com>
2020-07-15 14:56:56 -0700

    Add employee working

--- index.js
+++ index.js
@@ -154,12 +154,67 @@ function viewRoles() {
 }
 
 function addNewEmployee() {
-    start();
+    connection.query("SELECT * FROM role", (err, roleRes) => {
+        if (err) {
+            throw err;
+        }
+        const roleNames = roleRes.map((row) => row.title);
+
+        connection.query("SELECT * FROM employee", (err, managerRes) => {
+
+            const employeeNames = managerRes.map((row) => row.last_name);
+
+            inquirer
+                .prompt([
+                    {
+                        name: "firstName",
+                        type: "input",
+                        message: "What is the new employee's first name?",
+                    },
+                    {
+                        name: "lastName",
+                        type: "input",
+                        message: "What is the new employee's last name?",
+                    },
+                    {
+                        name: "role",
+                        type: "rawlist",
+                        choices: roleNames,
+                        message: "What is the new employee's role?",
+                    },
+                    {
+                        name: "manager",
+                        type: "rawlist",
+                        choices: employeeNames,
+                        message: "Who is the new employee's manager?",
+                    }
+                ]).then((answers) => {
+                    const chosenRole = roleRes.find((row) => row.title === answers.role);
+                    const chosenManager = managerRes.find((row) => row.last_name === answers.manager);
+
+                    var query = "INSERT INTO employee SET ?";
+                    connection.query(query,
+                        {
+                            first_name: answers.firstName,
+                            last_name: answers.lastName,
+                            role_id: chosenRole.id,
+                            manager_id: chosenManager.id
+                        },
+                        function (err) {
+                            if (err) throw err;
+
+                            console.log(answers.firstName + " " + answers.lastName + " employee added!");
+
+                            start();
+                        });
+                });
+        });
+    });
 }
 
 function viewEmployees() {
     var query = `select CONCAT(A.first_name, " ", A.last_name) AS name, title, CONCAT(B.first_name, " ", B.last_name) as manager 
-    from employee A inner join role ON A.role_id = role.id left join employee B on a.manager_id = b.id`;
+    from employee A inner join role ON A.role_id = role.id left join employee B on A.manager_id = B.id`;
     connection.query(query, function (err, res) {
         if (err) throw err;
         console.table(res);
